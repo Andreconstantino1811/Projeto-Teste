d2cdf485e105835d3db262011ca0416f970c11ac
Andreconstantino1811 <99025644+Andreconstantino1811@users.noreply.github.com>
2022-02-04 10:29:31 -0300

    Add files via upload

---
+++ Rocket.js
@@ -0,0 +1,29 @@
+import React, { useState, Component, memo } from 'react';
+import { shallowEqualObjects as _isEqual} from 'shallow-equal';
+import RocketCore from './RocketCore';
+
+export const FunctionalRocket = memo(() => {
+  const [initialLaunchTime] = useState(Date.now());
+
+  return <RocketCore initialLaunchTime={initialLaunchTime} />;
+});
+
+export class ClassRocket extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initialLaunchTime: Date.now()
+    };
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return !_isEqual(nextProps, this.props);
+  }
+
+  render() {
+    const { initialLaunchTime } = this.state;
+
+    return <RocketCore initialLaunchTime={initialLaunchTime} />;
+  }
+}
